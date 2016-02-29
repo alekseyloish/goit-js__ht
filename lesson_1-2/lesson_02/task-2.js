3bb534986822c795616daa65ed177edcc2d5a471
Aleksey Loish <aleksey.loish@gmail.com>
2016-02-29 22:16:44 +0200

    added task-2

--- lesson_1-2/lesson_02/task-2.js
+++ lesson_1-2/lesson_02/task-2.js
@@ -1,3 +1,27 @@
 (function(){
     
+    var listOfNames = [];
+    var enterOk = false;
+
+    for (var i = 0; i < 5; i++) {
+        listOfNames[i] = prompt('Заполните массив именами:');
+    }
+
+    console.log(listOfNames);
+
+    var userName = prompt('Введите имя пользователя:');
+
+    for (var i = 0; i < listOfNames.length; i++) {
+
+        if (userName == listOfNames[i]) {
+            enterOk = true;
+            alert(userName + ', вы успешно вошли');
+        }
+
+    }
+
+    if (!enterOk) {
+        alert('Такого пользователя нет в базе');
+    }
+
 }());
