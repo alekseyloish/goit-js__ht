ec02c43e1c47543d68976f1c32b22218ea4a612f
Aleksey Loish <aleksey.loish@gmail.com>
2016-03-23 12:48:19 +0200

    lesson_03-04

--- lesson_3-4/app/js/scripts.js
+++ lesson_3-4/app/js/scripts.js
@@ -34,6 +34,22 @@ var app = {
             element.innerHTML = options.text;
         }
 
+        if (options.inputType) {
+            element.setAttribute('type', options.inputType);
+        }
+
+        if (options.name) {
+            element.setAttribute('name', options.name);
+        }
+
+        if (options.id) {
+            element.setAttribute('id', options.id);
+        }
+
+        if (options.value) {
+            element.setAttribute('value', options.value);
+        }
+
         if (options.parentName) {
             options.parentName.appendChild(element);
         } else {
@@ -52,10 +68,83 @@ var app = {
         });
     },
 
+    generateList: function(parent, amountQuestion, amountAnswer) {
+        var list = this.createElement({
+            tagName: 'ol',
+            className: 'list-group',
+            parentName: parent
+        });
+
+        // Вывод списка вопросов
+        for (var i = amountQuestion; i > 0; i--) {
+            var listItem = this.createElement({
+                tagName: 'li',
+                className: 'list-group-item',
+                parentName: list
+            });
+
+            this.createElement({
+                tagName: 'h4',
+                className: 'title',
+                text: 'Вопрос № ' + (amountQuestion - i + 1),
+                parentName: listItem
+            });
+
+            var checkboxGroupContainer = this.createElement({
+                tagName: 'div',
+                className: 'checkbox-group',
+                parentName: listItem
+            });
+
+            // Вывод списка вариантов ответа на вопрос
+            for (var j = amountAnswer-1; j >= 0; j--) {
+                var checkboxContainer = this.createElement({
+                    tagName: 'div',
+                    className: 'checkbox',
+                    parentName: checkboxGroupContainer
+                });
+
+                var label = this.createElement({
+                    tagName: 'label',
+                    //text: 'Вариант ответа № ' + (amountAnswer - j),
+                    parentName: checkboxContainer
+                });
+
+                this.createElement({
+                    tagName: 'input',
+                    inputType: 'checkbox',
+                    name: 'answer',
+                    id: 'answer' + (amountQuestion - i) + '_' + (amountAnswer - j),
+                    parentName: label
+                });
+
+                this.createElement({
+                    tagName: 'span',
+                    text: 'Вариант ответа № ' + (amountAnswer - j),
+                    parentName: label
+                });
+            }
+        }
+    },
+
     generateForm: function(parent) {
-        this.createElement({
+        var form = this.createElement({
             tagName: 'form',
             className: 'form-quiz center-block',
+            parentName: parent
+        });
+
+        this.generateList(form, 3, 4);
+
+        this.generateButton(form);
+    },
+
+    generateButton: function(parent) {
+        this.createElement({
+            tagName: 'input',
+            inputType: 'submit',
+            value: 'Проверить мои результаты',
+            className: 'center-block btn btn-primary btn-lg',
             parentName: parent
         });
     },
@@ -67,9 +156,11 @@ var app = {
         });
 
         this.generateHeading(container);
-        this.generateForm(container);
-    }
 
+        this.generateForm(container)
+    }
 };
 
+
 app.generatePage();
+
